16c0a5f84c8cf050c55b759c7b7a640d4742b5b5
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-10-11 14:56:06 +0200

    nu_vid13_maken_rest_af

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const dancing = document.getElementById("js--dancing")
+const lato = document.getElementById("js--lato")
+const merriweather = document.getElementById("js--merriweather")
+const input = document.getElementById("js--input")
+
+
+
+
+input.onchange = function(event){
+dancing.innerText = event.target.value;
+lato.innerText  = event.target.value;
+merriweather.innerText  = event.target.value;
+
+}
