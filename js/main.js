057ca08bd1b395966986e791b49463dabb8aeed0
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-10-14 00:39:12 +0200

    opdracht3_af

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// java script for input change // 
 const dancing = document.getElementById("js--dancing")
 const lato = document.getElementById("js--lato")
 const merriweather = document.getElementById("js--merriweather")
@@ -6,9 +7,22 @@ const input = document.getElementById("js--input")
 
 
 
-input.onchange = function(event){
-dancing.innerText = event.target.value;
-lato.innerText  = event.target.value;
-merriweather.innerText  = event.target.value;
+input.onchange = function (event) {
+    dancing.innerText = event.target.value;
+    lato.innerText = event.target.value;
+    merriweather.innerText = event.target.value;
 
+}
+
+// javascript for favorites //
+
+const hearts = document.getElementsByClassName("heart");
+for (let i = 0; i < hearts.length; i++) {
+    hearts[i].onclick = function () {
+
+        for (let i = 0; i < hearts.length; i++) {
+            hearts[i].children[0].classList = "fa-regular fa-heart heart__logo";
+        }
+        hearts[i].children[0].classList = "fa-solid fa-heart heart__logo";
+    }
 }
